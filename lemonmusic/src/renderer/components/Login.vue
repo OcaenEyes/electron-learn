5f1f42c100e7c9f674e94c515562ac570db39fba
OcaenEyes <1450136519@qq.com>
2019-07-06 20:36:52 +0800

    Update Login.vue

--- lemonmusic/src/renderer/components/Login.vue
+++ lemonmusic/src/renderer/components/Login.vue
@@ -3,15 +3,12 @@
         <img class="welcome" src="">
         <div class="login">
             <div class="login_user">
-                <input type="text" placeholder="请输入用户名／邮箱／手机号">
+                <sapn>账户：</sapn><input type="text" placeholder="请输入用户名／邮箱／手机号">
             </div>
             <div class="login_password">
-                <input type="password" placeholder="请输入密码">
+                <span>密码：</span><input type="password" placeholder="请输入密码">
             </div>
-            <div class="login_button">
-                <input type="button" value="登录">
-
-            </div>
+            <input  class="login_button" type="button" value="登录">
         </div>
 
     </div>
@@ -34,11 +31,37 @@
         background-color: aquamarine;
     }
     .welcome {
+        height: 200px;
         background-color: bisque;
     }
     .login {
-        height: 60%;
-        background-color: aquamarine;
+        height: 200px;
+        background-color: red;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        align-items: center;
+        font-size: 14px;
+        font-family: sans-serif;
+    }
+    .login_user{
+        margin-top: 20px;
+        /* height: 40px; */
+        width: 300px;
+    }
+    .login_password {
+        /* height: 40px; */
+        margin-top: 20px;
+        width: 300px;
+    }
+    .login_button{
+        margin-top: 20px;
+        width: 100px;
+        height: 30px;
+        background-color: aqua;
+        border-radius: 15px;
+        border: 0px;
+
     }
 </style>
 
