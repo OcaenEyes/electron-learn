aebc2c9bc235041e31813dabe3fb255ce680f7f8
OcaenEyes <1450136519@qq.com>
2019-07-06 09:51:57 +0800

    Create Login.vue

---
+++ lemonmusic/src/renderer/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="login-page">
+        <img class="welcome" src="">
+        <div class="login">
+            <div class="login_user">
+
+            </div>
+            <div class="login_password">
+
+            </div>
+            <div class="login_button">
+                <input type="button" value="登录">
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'login',
+        data () {
+            return {
+                inputType :'password',
+            }
+        },
+    }
+      
+</script>
+
+<style>
+    body {
+        background-color: aquamarine;
+    }
+    .welcome {
+        background-color: bisque;
+    }
+    .login {
+        height: 60%;
+        background-color: aquamarine;
+    }
+</style>
+
