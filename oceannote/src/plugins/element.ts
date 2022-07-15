2720dff2ccd39d7258a7fdb0ac6c34a8416aed67
OcaenEyes <1450136519@qq.com>
2022-07-16 00:52:38 +0800

    master

---
+++ oceannote/src/plugins/element.ts
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion:
+ * @version: V0.0.1
+ * @Author: OCEAN.GZY
+ * @Date: 2022-07-16 00:25:44
+ * @LastEditors: OCEAN.GZY
+ * @LastEditTime: 2022-07-16 00:25:48
+ */
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+export default (app: any) => {
+  app.use(ElementPlus)
+}
